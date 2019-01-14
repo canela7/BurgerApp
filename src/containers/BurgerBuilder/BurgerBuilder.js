72da73e32b8df1a80775ee6c8855ad489b8ecf54
Brian Canela <briancanela@Brians-iMac.socal.rr.com>
2019-01-13 17:19:33 -0800

    implemented the backdrop component to close the modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,8 +85,13 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
 
   render(){
+
     const disabledInfo = {
       ...this.state.ingredients
     };
@@ -96,20 +101,24 @@ class BurgerBuilder extends Component {
     }
 
     return(
+
       <Aux>
 
-          <Modal show={this.state.purchasing}>
+          <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+
             <OrderSummary ingredients={this.state.ingredients}/>
+
           </Modal>
 
           <Burger ingredients={this.state.ingredients} />
+
           <BuildControls
             ingredientAdded={this.addIngredientHandler}
             ingredientRemoved={this.removeIngredientHandler}
             disabled={disabledInfo}
             purchasable={this.state.purchasable}
             ordered={this.purchaseHandler}
-            price={this.state.totalPrice}/>
+            price={this.state.totalPrice} />
 
       </Aux>
     );
